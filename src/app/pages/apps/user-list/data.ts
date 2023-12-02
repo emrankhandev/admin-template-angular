fc16a0ff2d63064ee860632216a8c64c703605df
Emran <atdimla@gmail.com>
2023-12-02 10:02:51 +0600

    first commit

---
+++ src/app/pages/apps/user-list/data.ts
@@ -0,0 +1,82 @@
+const tableData = [
+  {
+    id: 1,
+    image: 'assets/images/users/avatar-2.jpg',
+    name: 'Phyllis Gatlin',
+    position: 'UI/UX Designer',
+    email: 'phyllisgatlin@Iqra.com',
+    tags: ['Photoshop', 'illustrator']
+  }, {
+    id: 2,
+    image: 'assets/images/users/avatar-1.jpg',
+    name: 'James Nix',
+    position: 'Frontend Developer',
+    email: 'jamesnix@Iqra.com',
+    tags: ['Html', 'Css', '2 + more']
+  }, {
+    id: 3,
+    image: 'assets/images/users/avatar-3.jpg',
+    name: 'Darlene Smith',
+    position: 'Backend Developer',
+    email: 'darlenesmith@Iqra.com',
+    tags: ['Php', 'Java', 'Python']
+  }, {
+    id: 4,
+    image: 'assets/images/users/avatar-3.jpg',
+    name: 'William Swift',
+    position: 'Full Stack Developer',
+    email: 'williamswift@Iqra.com',
+    tags: ['Ruby', 'Php', '2 + more']
+  }, {
+    id: 5,
+    image: 'assets/images/users/avatar-3.jpg',
+    name: 'Kevin West',
+    position: 'Frontend Developer',
+    email: 'kevinwest@Iqra.com',
+    tags: ['Html', 'Css', '2 + more']
+  }, {
+    id: 6,
+    image: 'assets/images/users/avatar-6.jpg',
+    name: 'Tommy Hayes',
+    position: 'UI/UX Designer',
+    email: 'tommyhayes@Iqra.com',
+    tags: ['Photoshop', 'illustrator']
+  }, {
+    id: 7,
+    image: 'assets/images/users/avatar-8.jpg',
+    name: 'Diana Owens',
+    position: 'Graphic Designer',
+    email: 'dianaowens@Iqra.com',
+    tags: ['Photoshop', 'illustrator']
+  }, {
+    id: 8,
+    image: 'assets/images/users/avatar-9.jpg',
+    name: 'Paul Sanchez',
+    position: 'Angular Developer',
+    email: 'paulsanchez@Iqra.com',
+    tags: ['Php', 'Javascript']
+  }, {
+    id: 9,
+    image: 'assets/images/users/avatar-9.jpg',
+    name: 'Peter Dryer',
+    position: 'Web Designer',
+    email: 'peterdryer@Iqra.com',
+    tags: ['Html', 'Css', '2 + more']
+  }, {
+    id: 10,
+    image: 'assets/images/users/avatar-4.jpg',
+    name: 'Gerald Moyer',
+    position: 'Backend Developer',
+    email: 'geraldmoyer@Iqra.com',
+    tags: ['Php', 'Javascript']
+  }, {
+    id: 11,
+    image: 'assets/images/users/avatar-2.jpg',
+    name: 'Gail McGuire',
+    position: 'Backend Developer',
+    email: 'gailmcGuire@Iqra.com',
+    tags: ['Php', 'Javascript']
+  }
+];
+
+export { tableData };
